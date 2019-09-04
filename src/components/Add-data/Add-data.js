a01481e63384a74ab6441043eae61b710145f563
Timothy Putzke <tsputzke@gmail.com>
2019-09-03 20:51:01 -0400

    Implement add-data

--- src/components/Add-data/Add-data.js
+++ src/components/Add-data/Add-data.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
+import moment from 'moment'
 import './Add-data.css';
 
 export default class AddData extends Component {
+  state = {
+    user: 1,
+    room_id: 1,
+  }
+
+  handleAddData = e => {  
+    e.preventDefault();
+
+    const date_added = e.target.date_added.value;
+    const temperature = e.target.temperature.value;
+    const rh = e.target.rh.value;
+    const co2 = e.target.co2.value;
+    const light = e.target.light.value;
+    const comments = e.target.comments.value;
+
+    // //validate the input
+    // if (user_name === "") {
+    //   alert("Please enter username");
+    // } else if (password === "") {
+    //   alert("Please enter password");
+    // }
+
+    const newDataObject = {
+      room_id: this.state.room_id,
+      date_added: date_added,
+      temperature: temperature,
+      rh: rh,
+      co2: co2,
+      light: light,
+      comments: comments
+    };
+
+    fetch(`http://localhost:8000/api/room-data`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(newDataObject)
+    })
+      // Reload page if call is successfull
+      .then(window.location.reload())
+      // If call fails
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className='add-data'>
         <section id="add-data-section">
           <h1>Add Data</h1>
-          <form id="add-data-form">
+          <form id="add-data-form" onSubmit={this.handleAddData}>
             <div id="inputs">
               <div>
-                <label htmlFor="date">Date: </label>
-                <input type="text" name="date" required />
+                <label htmlFor="date_added">Date: </label>
+                <input type="date" name="date_added" defaultValue={moment().format("YYYY-MM-DD")} required />
               </div>
               <div>
                 <label htmlFor="temperature">Temperature (C): </label>
-                <input type="text" name="temperature" />
+                <input type="text" name="temperature"/>
               </div>
               <div>
-                <label htmlFor="rh">rH (%): </label>
+                <label htmlFor="rh">RH (%): </label>
                 <input type="text" name="rh" />
               </div>
               <div>
@@ -26,14 +72,14 @@ export default class AddData extends Component {
                 <input type="text" name="co2" />
               </div>
               <div>
-                <label htmlFor="light">Light (umol/m2/s): </label>
+                <label htmlFor="light">Light (ppfd): </label>
                 <input type="text" name="light" />
               </div>
             </div>
             <div>
               <label htmlFor="comments">Comments: </label>
               <br />
-              <input id="comments-textbox" type="text" name="comments" />
+              <textarea name="comments" id="comments-textbox" rows="10" cols="30"></textarea> 
             </div>
             <button type="submit">Submit</button>
           </form>
