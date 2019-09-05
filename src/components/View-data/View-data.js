20df1e3c1f5857267084cec049bc2370f0bf5102
Timothy Putzke <tsputzke@gmail.com>
2019-09-04 20:21:55 -0400

    Implement view data logic

--- src/components/View-data/View-data.js
+++ src/components/View-data/View-data.js
@@ -2,7 +2,41 @@ import React, { Component } from 'react'
 import '../View-data/View-data.css'
 
 export default class ViewData extends Component {
+  state = {
+    dateArray: [],
+    room_id: 1,
+    room: 'Demo Room'
+  }
+
+  componentWillMount() {
+    // Get room_data for a given room
+    fetch(`http://localhost:8000/api/room-data/${this.state.room_id}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      // If call is successfull
+      .then(res => res.json() )
+      .then(res => {
+        let data = [];
+        res.map(date => data.push(date))
+        this.setState({data: data})
+      })
+      .then(res => {
+        // Sort dateArray by date_added
+        const dateArray = this.state.data.sort((a,b) => (a.date_added > b.date_added ? 1 : -1))
+        this.setState({dateArray: dateArray})
+      })
+      // If call fails
+      .catch(err => console.log(err));
+  };
+
   render() {
+    // Create table row for each date from the dataArray in state
+    const populateTable = this.state.dateArray.reverse().map((date, i) => {
+      return <tr key={i}><th>{date.date_added}</th><td>{date.temperature}</td><td>{date.rh}</td><td>{date.co2}</td><td>{date.light}</td><td><pre>{date.comments}</pre></td></tr>}) 
+
     return (
       <section className='view-data'>
         <h1>View Data</h1>
@@ -17,46 +51,7 @@ export default class ViewData extends Component {
               <th> Light </th>
               <th> Comments </th>
             </tr>
-            <tr>
-              <td>04/01/2019</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-            </tr>
-            <tr>
-              <td>04/01/2019</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-            </tr>
-            <tr>
-              <td>04/01/2019</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-            </tr>
-            <tr>
-              <td>04/01/2019</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-            </tr>
-            <tr>
-              <td>04/01/2019</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-              <td>25</td>
-            </tr>
+            {populateTable}
           </tbody>
         </table>
       </section>
