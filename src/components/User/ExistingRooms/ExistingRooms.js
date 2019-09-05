dc8695d96a418ceb3afb6b65429254cac5c2f9a0
Timothy Putzke <tsputzke@gmail.com>
2019-09-04 20:23:44 -0400

    Update link to /show-room (temporary)

--- src/components/User/ExistingRooms/ExistingRooms.js
+++ src/components/User/ExistingRooms/ExistingRooms.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 export default class ExistingRooms extends Component {
   render(){
     const rooms = this.props.rooms.map(function(room, i) {
-      return <li key={i}>{room.room_name}</li>
+      return <li key={i}><Link to="/show-room">{room.room_name}</Link></li>
     })
 
     return(
       <section id="existing-rooms">
         <h2>Choose Existing Room:</h2>
         <ul>
-          <li><Link to="/user/1/room/1">Example Room</Link></li>
+          <li><Link to="/show-room">Example Room</Link></li>
           {rooms}
         </ul>
       </section>
